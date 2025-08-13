85bcd2c3fc50ff124086ed55dd71bd51c260df04
kpiyush25 <piyushkumar.94528@gmail.com>
2025-08-14 01:53:38 +0530

    Try connecting to netlify form

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log('Forms with data-netlify:', document.querySelectorAll('form[data-netlify]'));
     console.log('Forms in .contact-form:', document.querySelectorAll('.contact-form form'));
 
-    const contactForm = document.querySelector('form[name="contact"]');
+    const contactForm = document.querySelector('form[data-netlify="true"]');
     console.log('Selected form:', contactForm);
     console.log('Form has data-netlify:', contactForm?.hasAttribute('data-netlify'));
 
